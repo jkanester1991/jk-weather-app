02f637b04b19b0bbc2885c7257922faa17e81576
jkanester1991 <jkanester@gmail.com>
2025-12-17 13:27:38 -0800

    created js to replace temperature and city elements with searched data, added city on page load as Kelowna

--- src/script.js
+++ src/script.js
@@ -1,11 +1,27 @@
+function refreshWeather(response) {
+  console.log(response.data);
+  let cityTemp = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityName = document.querySelector("#city-name");
+
+  cityName.innerHTML = response.data.city;
+  cityTemp.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput.value);
 
-  let cityName = document.querySelector("#city-name");
-  cityName.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kelowna");
