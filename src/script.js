029d37f3a30a8d9254930a4dac26dbf2b88d2a70
jkanester1991 <jkanester@gmail.com>
2025-12-22 09:50:52 -0800

    removed hardcoded html, inject the forecast html from js

--- src/script.js
+++ src/script.js
@@ -62,7 +62,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="forecast-column">
+            <div class="forecast-day">${day}</div>
+            <div class="forecast-icon">🌤️</div>
+            <div class="forecast-temps">
+              <span class="forecast-high">18°</span>
+              <span class="forecast-low">14°</span>
+            </div>
+          </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Kelowna");
+displayForecast();
